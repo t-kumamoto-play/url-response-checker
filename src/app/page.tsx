15478072e7bd75656684b3410b55bbdb1c0ce908
tsuyoshi.kumamoto <tsuyoshi.kumamoto@play.jp>
2024-12-23 10:34:10 +0900

    ボタンを押下して判定するように修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,47 @@
 "use client"
-import React from "react";
+import React, { useState } from "react";
 
 export default function Home() {
-  // const load = async (_url: string) => {
-  //   var xhr;
-  //   xhr = new XMLHttpRequest();
-  //   xhr.open("HEAD", _url, false);
-  //   xhr.send(null);
-  //   return xhr;
-  // }
-  const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    // const res = await load(value);
-    // console.log({ res })
+  const [value, setValue] = useState<string>('');
+  const [isOK, setIsOK] = useState<boolean>(false);
+  const [statusCode, setStatusCode] = useState<number>();
 
-    fetch(value, { mode: "cors" }).then((res) => {
-      console.log({ res });
+  // 入力欄の値取得
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setValue(value);
+  };
+
+  // ボタン押下時にチェック
+  const handleCheckButton = async () => {
+    if (!value) return null;
+    await fetch(value, { mode: "cors" }).then((response) => {
+      console.log({ response })
+      setIsOK(response.ok);
+      setStatusCode(response.status);
     }).catch((e) => {
       console.error("Error:", e);
     });
+  }
 
-  };
+  /**
+   * ボタン押下で判定するが、入力と同時（コピペ）で判定する際はonhandleChange内で判定処理を入れる方が良い
+   * 上記手段で判定するときはある程度setTuimeout等でsleep設けて判定した方がfetch叩きまくらなくなるので良いかも
+   */
+
   return (
     <div>
       <h1>URL check</h1>
-      <input onChange={onChange} type="text" />
-
-      <ul>
-      </ul>
+      <div style={{ width: "100%" }}>
+        <div style={{ display: "block" }}>
+          <input onChange={handleChange} type="text" style={{ width: "50%", display: "block" }} />
+          <button onClick={handleCheckButton}>Check</button>
+        </div>
+        <ul>
+          <li>OK: {isOK ? 'OK' : 'NG'}</li>
+          <li>Status: {statusCode ? statusCode : "---"}</li>
+        </ul>
+      </div>
     </div>
   );
 }
